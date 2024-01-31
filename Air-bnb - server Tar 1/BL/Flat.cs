f2c0d4ff1ddd33b13109bd131058a2b4d33da0ca
Ben Shuan <benshuan@gmail.com>
2024-01-31 22:58:17 +0200

    Add finishes

---
+++ Air-bnb - server Tar 1/BL/Flat.cs
@@ -0,0 +1,61 @@
+﻿namespace Air_bnb.BL
+{
+    public class Flat
+    {
+        string id,city,address;
+        double price;
+        int numberOfRooms;
+        static List<Flat> FlatList = new List<Flat>();
+
+        public Flat(string id, string city, string address, double price, int numberOfRooms)
+        {
+            Id = id;
+            City = city;
+            Address = address;
+            NumberOfRooms = numberOfRooms;
+            Price = price;
+        }
+
+        public Flat()
+        {
+        }
+
+        public string Id { get => id; set => id = value; }
+        public string City { get => city; set => city = value; }
+        public string Address { get => address; set => address = value; }
+        public int NumberOfRooms { get => numberOfRooms; set => numberOfRooms = value; }
+        public double Price { get => price; set => price = Discount(value); }
+
+
+        public bool Insert()
+        {
+            if (!FlatList.Exists(flat => flat.id == this.id))
+            {
+                FlatList.Add(this);
+                return true;
+            }
+            return false;
+
+        }
+
+        static public List<Flat> Read()
+        {
+            return FlatList;
+        }
+        
+        static public Flat Read(string id)
+        {
+            return FlatList.Find(item => item.Id == id)?? new Flat() ;
+        }
+
+        double Discount(double val)
+        {
+            return (this.NumberOfRooms > 1 && val > 100) ? val * 0.9 : val;
+        }
+
+        static public List<Flat> GetMaxPriceInCity(string city, int price)
+        {
+            return FlatList.FindAll(flat => flat.City == city && flat.price < price);
+        }
+    }
+}
---
+++ Air-bnb - server Tar 1/BL/Vacation.cs
@@ -0,0 +1,67 @@
+﻿namespace Air_bnb.BL
+{
+    public class Vacation
+    {
+        string id, userId, flatId;
+        DateTime startDate, endDate;
+
+        static List<Vacation> vacationsList = new();
+
+        public Vacation(string id, string userId, string flatId, DateTime startDate, DateTime endDate)
+        {
+            Id = id;
+            UserId = userId;
+            FlatId = flatId;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        public Vacation()
+        {
+        }
+
+        public string Id { get => id; set => id = value; }
+        public string UserId { get => userId; set => userId = value; }
+        public string FlatId { get => flatId; set => flatId = value; }
+        public DateTime StartDate { get => startDate; set => startDate = value; }
+        public DateTime EndDate { get => endDate; set => endDate = value; }
+
+        public bool SameDatesAndFlat(Vacation vac1, Vacation vac2)
+        {
+            return !(vac1.EndDate <= vac2.StartDate || vac2.EndDate <= vac1.StartDate) && vac1.FlatId == vac2.FlatId;
+        }
+
+        public bool Insert()
+        {
+
+            if (vacationsList.TrueForAll(vac => vac.Id != this.Id && !SameDatesAndFlat(vac,this)))
+            {
+                vacationsList.Add(this);
+                return true;
+            }
+            return false;
+        }
+
+        static public List<Vacation> Read()
+        {
+            return vacationsList;
+        }
+        
+        static public Vacation? Read(string id)
+        {
+            return vacationsList.Find(v => v.Id == id);
+        }
+
+
+        static bool BetweenDates(DateTime start, DateTime end, DateTime check)
+        {
+            return start <= check && check <= end;
+        }
+
+        static public List<Vacation> GetByDates(DateTime startDate, DateTime endDate)
+        {
+            return vacationsList.FindAll(vac => BetweenDates(startDate, endDate,vac.startDate)|| BetweenDates(startDate, endDate, vac.EndDate));
+        }
+    }
+
+}
