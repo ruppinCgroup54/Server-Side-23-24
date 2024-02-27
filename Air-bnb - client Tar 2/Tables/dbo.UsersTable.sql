4ac14a01b8f709f3b99bcd5de6b599bf55e40de5
RoniYehoshua <roni6531607@gmail.com>
2024-02-27 17:10:04 +0200

    Database

---
+++ Air-bnb - client Tar 2/Tables/dbo.UsersTable.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UsersTable] (
+    [firstName]  NVARCHAR (20) NOT NULL,
+    [familyName] NVARCHAR (20) NOT NULL,
+    [email]      NVARCHAR (50) NOT NULL,
+    [password]   NVARCHAR (20) NOT NULL,
+    PRIMARY KEY CLUSTERED ([email] ASC)
+);
+
