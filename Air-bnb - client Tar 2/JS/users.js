5001e484e6b8959d70d58029647bc3a177777322
Ben Shuan <benshuan@gmail.com>
2024-02-28 14:59:38 +0200

    .

--- Air-bnb - client Tar 2/JS/users.js
+++ Air-bnb - client Tar 2/JS/users.js
@@ -1,66 +1,65 @@
 $(document).ready(function () {
+  //server = (window.location.hostname === "localhost" || window.location.hostname === `7014` ? `https://localhost:7014/` : `https://proj.ruppin.ac.il/cgroup54/test2/tar1/`);
+  server = "https://localhost:7014/";
+  $("#userForm").submit(addNewUser);
 
-    //server = (window.location.hostname === "localhost" || window.location.hostname === `7014` ? `https://localhost:7014/` : `https://proj.ruppin.ac.il/cgroup54/test2/tar1/`);
-    server = 'https://localhost:7014/';
-    $("#userForm").submit(addNewUser);
-
-    $("#loginForm").submit(loginUser);
+  $("#loginForm").submit(loginUser);
 });
 
 function addNewUser(e) {
-    let data = e.target;
+  let data = e.target;
 
-    let newUser = {
-        FirstName: data.firstName.value,
-        FamilyName: data.familyName.value,
-        Email: data.email.value,
-        Password: data.password.value,
-    };
-    ajaxCall(
-        "POST",
-        server + "api/Users",
-        JSON.stringify(newUser),
-        sInsertCB,
-        eInsertCB
-    );
-    e.target.reset();
-    return false;
+  let newUser = {
+    FirstName: data.firstName.value,
+    FamilyName: data.familyName.value,
+    Email: data.email.value,
+    Password: data.password.value,
+  };
+  ajaxCall(
+    "POST",
+    server + "api/Users",
+    JSON.stringify(newUser),
+    sInsertCB,
+    eInsertCB
+  );
+  e.target.reset();
+  return false;
 }
 
 function sInsertCB(res) {
-    console.log("res", res);
-    swal("User has been registered!", "Great Job", "success");
+  console.log("res", res);
+  swal("User has been registered!", "Great Job", "success");
 }
 function eInsertCB(err) {
-    console.log("err");
-    alert(err.responseText);
+  console.log("err");
+  alert(err.responseText);
 }
 
 function loginUser(e) {
-    let data = e.target;
-
-    let loginUser = {
-        FirstName: "",
-        FamilyName: "",
-        Email: data.email.value,
-        Password: data.password.value,
-    };
-    ajaxCall(
-        "POST",
-        server + "api/Users/login",
-        JSON.stringify(loginUser),
-        sLInsertCB,
-        eLInsertCB
-    );
-    e.target.reset();
-    return false;
+  let data = e.target;
+  e.preventDefault();
+  let loginUser = {
+    FirstName: "",
+    FamilyName: "",
+    Email: data.email.value,
+    Password: data.password.value,
+  };
+  ajaxCall(
+    "POST",
+    server + "api/Users/login",
+    JSON.stringify(loginUser),
+    sLInsertCB,
+    eLInsertCB
+  );
+  e.target.reset();
+  return false;
 }
 
 function sLInsertCB(res) {
-    console.log("res", res);
-    //swal("User has been registered!", "Great Job", "success");
+  console.log("res", res);
+  //swal("User has been registered!", "Great Job", "success");
 }
 function eLInsertCB(err) {
-    console.log("err");
-    alert(err.responseText);
+  console.log("err");
+  alert(err.responseText);
 }
